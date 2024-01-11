3a0d745d1a23c7e5f51c06fc3cc04ce8dd08feed
erkanzzafer <zzafererkann@gmail.com>
2024-01-11 20:32:18 +0300

    quiz questions shown by click to btn

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const exitBtn = document.querySelector(".exit-btn");
 const main = document.querySelector(".main");
 const continueBtn = document.querySelector(".continue-btn");
 const quizSection = document.querySelector(".quiz-section");
+const quizBox = document.querySelector(".quiz-box");
 
 
 startBtn.onclick = () => {
@@ -16,8 +17,41 @@ exitBtn.onclick = () => {
     main.classList.remove("active");
 }
 
-continueBtn.onclick=()=>{
+continueBtn.onclick = () => {
     quizSection.classList.add("active");
     popupInfo.classList.remove("active");
     main.classList.remove("active");
+    quizBox.classList.add("active");
+    showQuestions(0);
 };
+
+let questionCount = 0;
+const nextBtn = document.querySelector(".next-btn");
+
+
+nextBtn.onclick = () => {
+    if (questionCount < questions.length - 1) {
+        questionCount++;
+        showQuestions(questionCount);
+    }
+}
+
+
+const optionList = document.querySelector(".option-list");
+
+
+
+//getting questions and options from array
+function showQuestions(index) {
+    const questionText = document.querySelector(".question-text");
+    questionText.textContent = `${questions[index].numb}.${questions[index].question}`;
+
+    let optionTag = `
+    <div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+
+}
